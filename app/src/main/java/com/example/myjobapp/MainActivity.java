2065bc419337410638f4de3596cdb9bb7188d337
Rémy <remy.napias@next-u.fr>
2021-02-10 13:51:05 +0100

    Create ButtonListener to language activity

--- app/src/main/java/com/example/myjobapp/MainActivity.java
+++ app/src/main/java/com/example/myjobapp/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.myjobapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button mButtonHomePage = findViewById(R.id.searchButton);
+        mButtonHomePage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, language.class);
+                startActivity(intent);
+            }
+        });
     }
 }
